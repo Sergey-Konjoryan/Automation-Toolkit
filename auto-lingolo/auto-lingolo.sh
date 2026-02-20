a292ce50f4de4d1a4cf1ca5d83a16288d0c4f2b9
LuX <sergeykonjoryangg@gmail.com>
2026-02-20 22:24:17 +0400

    Add auto-lingolo.sh for Ligolo-ng interface setup
    This script automates the creation of a TUN interface for Ligolo-ng, ensuring it runs with root privileges and provides feedback during the setup process.

--- auto-lingolo/auto-lingolo.sh
+++ auto-lingolo/auto-lingolo.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
+# Title: Auto-Ligolo Interface Setup
+# Author: LuX
+# Description: Automates the creation of TUN interface for Ligolo-ng.
 
+if [[ $EUID -ne 0 ]]; then
+   echo "[-] This script must be run as root (use sudo)"
+   exit 1
+fi
+
+INTERFACE="ligolo"
+
+echo "[*] Starting Ligolo-ng interface setup..."
+
+ip tuntap add user $(logname) mode tun $INTERFACE
+echo "[+] TUN interface '$INTERFACE' created."
+
+ip link set $INTERFACE up
+echo "[+] Interface '$INTERFACE' is now UP."
+
+echo "[*] Setup complete. You can now start your proxy server."
